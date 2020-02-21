ecda5ad9b2af242332149518681831d355f4a38b
Yuda Ferry Mahendra <thegdads@gmail.com>
2020-02-21 08:31:54 +0700

    initial capacitor reactjs

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const title = 'EHW';
+  return (
+    <div class="container">
+      <div class="object">
+        <div class="subObject">
+          <h1>
+           {title}
+          </h1>
+          <h4>Enterprise Hello World</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
