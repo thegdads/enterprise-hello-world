4f405ecf747f204146ca0bcbbdf8591d8ab6b7fa
Yuda Ferry Mahendra <thegdads@gmail.com>
2020-02-21 08:59:12 +0700

    initial capacitor vuejs

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="object">
+        <div class="subObject">
+          <h1>
+            {{title}}
+          </h1>
+          <h4>Enterprise Hello World</h4>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => {
+    return {
+      title: 'EHW'
+    }
+  }
+}
+</script>
+
+<style>
+.container, .object {
+  display: flex;
+  background-color: #f5f0df;
+}
+.container {
+  height:98vh;
+  justify-content: center;
+}
+.object {
+  align-items: center;
+}
+.subObject {
+  text-align: center;
+}
+h1 {
+  font-size: 5em;
+  text-decoration: underline;
+}
+h1, h4 {
+  margin: 0;
+}
+</style>
